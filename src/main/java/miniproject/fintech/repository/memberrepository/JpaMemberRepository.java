0f8785c7dc56af3b479c0d3f388d345cf8df78cb
hwang <hwangsangik@gmail.com>
2024-08-09 22:19:00 +0900

    rfactorAndTestCode

--- src/main/java/miniproject/fintech/repository/memberrepository/JpaMemberRepository.java
+++
@@ -1,7 +0,0 @@
-package miniproject.fintech.repository.memberrepository;
-
-import miniproject.fintech.domain.BankMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaMemberRepository extends JpaRepository<BankMember, Long> {
-}
--- src/main/java/miniproject/fintech/service/discount/Discount.java
+++
@@ -1,9 +0,0 @@
-package miniproject.fintech.service.discount;
-
-import miniproject.fintech.domain.BankMember;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Discount {
-    void discount(BankMember bankMember, int fee);
-}
