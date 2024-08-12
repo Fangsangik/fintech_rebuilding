29cbe4ef6f2bf9647694fe7980761cd80c2bedda
hwang <hwangsangik@gmail.com>
2024-08-12 21:47:44 +0900

    feat = "추후 thymeleaf로 변경시, DeleteMapping -> PostMapping으로 인식"

---
+++ src/main/java/miniproject/fintech/config/WebConfig.java
@@ -0,0 +1,25 @@
+package miniproject.fintech.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /*
+    Post 요청을 DELETE로 변환 -> HiddenHttpMethodFilter를 설정해야 함
+    HTML 폼에서 Post 요청을 DELET등 HTTP 메소드로 변환
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+}
