4f2b07a0348d04937c633ce80668ef3159b9a6b2
hwang <hwangsangik@gmail.com>
2024-08-22 20:42:49 +0900

    refactor = "Setter 추가"

--- src/main/java/miniproject/fintech/dto/BankMemberDto.java
+++ src/main/java/miniproject/fintech/dto/BankMemberDto.java
@@ -1,9 +1,6 @@
 package miniproject.fintech.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import miniproject.fintech.type.Grade;
 
 import javax.validation.constraints.Email;
@@ -17,6 +14,7 @@ import java.util.List;
 
 @Getter
 @Builder
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BankMemberDto {
