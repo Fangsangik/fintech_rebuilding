6af585ced95ddbc25762e4abc641b5e60bf8c253
hwangsang-ik <hwangsangik@gmail.com>
2024-08-31 00:48:24 +0900

    refactor = "log 추가, HTTP 메서드 Post를 GET으로 가져오는 문제 발생"

--- src/main/java/miniproject/fintech/controller/MemberRegisterController.java
+++ src/main/java/miniproject/fintech/controller/MemberRegisterController.java
@@ -2,23 +2,23 @@ package miniproject.fintech.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import miniproject.fintech.domain.BankMember;
 import miniproject.fintech.dto.BankMemberDto;
 import miniproject.fintech.error.CustomError;
 import miniproject.fintech.service.MemoryMemberService;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static miniproject.fintech.type.ErrorType.MEMBER_NOT_FOUND;
 
+@Slf4j
 @RestController
 @RequestMapping("/register")
 @RequiredArgsConstructor
@@ -28,7 +28,15 @@ public class MemberRegisterController {
 
     @PostMapping("/create")
     public ResponseEntity<BankMember> createMember(@Valid @RequestBody BankMemberDto bankMemberDto) throws CustomError {
-        BankMember newMember = memberService.createBankMember(bankMemberDto, new HashSet<>(Set.of("USER"))); // 기본 역할로 USER 추가
-        return ResponseEntity.status(HttpStatus.CREATED).body(newMember);
+            log.info("회원 생성 요청 수신: {}", bankMemberDto);
+
+        try {
+            BankMember newMember = memberService.createBankMember(bankMemberDto, new HashSet<>(Set.of("USER"))); // 기본 역할로 USER 추가
+            log.info("회원 생성 성공: {}", newMember);
+            return ResponseEntity.ok(newMember);
+        } catch (CustomError e) {
+            log.error("회원 생성 실패: {}", e.getMessage(), e);
+            throw e; // 예외를 다시 던져서 상위 레벨에서 처리하도록 할 수 있습니다.
+        }
     }
 }
