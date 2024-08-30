c7b48c7cbed1a234ca4058f4f08570103433f958
hwangsang-ik <hwangsangik@gmail.com>
2024-08-31 00:46:10 +0900

    feat = "HTTP 에러 함꼐 추가"

--- src/main/java/miniproject/fintech/type/ErrorType.java
+++ src/main/java/miniproject/fintech/type/ErrorType.java
@@ -13,11 +13,11 @@ public enum ErrorType {
     MUST_NOT_NULL("빈 값이면 안됩니다.", HttpStatus.BAD_REQUEST),
     SOURCE_ID_NOT_FOUND("보낼 아이디를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
     DESTINATION_ID_NOT_FOUND("받을 아이디를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    TRANSACTION_DELETE_FAILED("거래 삭제에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    TRANSACTION_DELETE_FAILED("거래 삭제에 실패했습니다.", HttpStatus.BAD_REQUEST), // 수정
     TRANSACTION_NOT_FOUND("거래를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    NOT_ALLOWED_ACCESS("잘못된 접근입니다.", HttpStatus.NOT_FOUND),
+    NOT_ALLOWED_ACCESS("잘못된 접근입니다.", HttpStatus.FORBIDDEN), // 수정
     PASSWORD_INCORRECT("비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
-    MEMBER_EXIST("존재하는 회원입니다.", HttpStatus.BAD_GATEWAY),
+    MEMBER_EXIST("존재하는 회원입니다.", HttpStatus.CONFLICT), // 수정
     ACCOUNT_ID_NOT_FOUND("계좌 아이디가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
     IN_CORRECT("잘못된 값입니다.", HttpStatus.BAD_REQUEST),
     GRADE_NOT_VIP("VIP 등급이 아닙니다.", HttpStatus.BAD_REQUEST),
