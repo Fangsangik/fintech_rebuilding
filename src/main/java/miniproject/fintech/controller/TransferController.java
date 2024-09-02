39478be85a013286f2024b686a92d48a2638eef3
Fangsangik <hwangsangik@gmail.com>
2024-09-03 00:39:09 +0900

    feat : "cache 및 일부 기능 추가"

--- src/main/java/miniproject/fintech/controller/TransferController.java
+++ src/main/java/miniproject/fintech/controller/TransferController.java
@@ -1,29 +1,54 @@
 package miniproject.fintech.controller;
 
 import lombok.RequiredArgsConstructor;
-import miniproject.fintech.domain.Transfer;
-import miniproject.fintech.dto.TransactionDto;
+import lombok.extern.slf4j.Slf4j;
 import miniproject.fintech.dto.TransferDto;
 import miniproject.fintech.service.TransferServiceImpl;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 @RequestMapping("/transfer")
 @RequiredArgsConstructor
-@PreAuthorize("hasRole('USER')")
 public class TransferController {
 
     private final TransferServiceImpl transferService;
 
+    @PreAuthorize("hasRole('USER')")
     @PostMapping("/process")
-    public ResponseEntity<TransferDto> processTransfer
-            (@RequestBody TransferDto transferDto) {
+    @CacheEvict(value = "transfersCache", allEntries = true)
+    public ResponseEntity<TransferDto> processTransfer(@RequestBody TransferDto transferDto) {
         TransferDto transfer = transferService.processTransfer(transferDto);
         return ResponseEntity.ok().body(transfer);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/{id}")
+    @Cacheable(value = "transfersCache", key = "#id")
+    public ResponseEntity<TransferDto> getTransferById(@PathVariable Long id) {
+        TransferDto transfer = transferService.getTransferById(id);
+        return ResponseEntity.ok().body(transfer);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/account/{accountId}")
+    @Cacheable(value = "transfersCache", key = "'account_' + #accountId")
+    public ResponseEntity<List<TransferDto>> getTransfersByAccountId(@PathVariable Long accountId) {
+        List<TransferDto> transfers = transferService.getTransfersByAccountId(accountId);
+        return ResponseEntity.ok().body(transfers);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/delete/{id}")
+    @CacheEvict(value = "transfersCache", key = "#id")
+    public ResponseEntity<String> deleteTransferById(@PathVariable Long id) {
+        transferService.deleteTransferById(id);
+        return ResponseEntity.ok("Transfer deleted successfully");
     }
 }
