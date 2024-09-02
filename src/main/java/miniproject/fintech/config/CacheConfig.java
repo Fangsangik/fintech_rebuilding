700d3361790fdb2dfa9790bccff2fef9e56adbe2
Fangsangik <hwangsangik@gmail.com>
2024-09-03 00:32:13 +0900

    feat : "cache 값 설정"

--- src/main/java/miniproject/fintech/config/CacheConfig.java
+++ src/main/java/miniproject/fintech/config/CacheConfig.java
@@ -1,17 +1,25 @@
 package miniproject.fintech.config;
 
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.cache.configuration.MutableCacheEntryListenerConfiguration;
+import javax.cache.spi.CachingProvider;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @EnableCaching // 캐시 기능을 활성화
 @Configuration
 public class CacheConfig {
 
     @Bean
-    public CacheManager cacheManager() { //캐시를 관리하는 역할
-        return new ConcurrentMapCacheManager("fintechCache"); //기본 메모리 기반 캐시 (heap 메모리 ㅈ당)
+    public CacheManager cacheManager() {
+        // 기본 메모리 내 캐시 매니저를 사용하여 "defaultCache"라는 이름의 캐시를 생성
+        return new ConcurrentMapCacheManager("MemberCache", "depositCache", "adminCache", "accountsCache", "transfersCache", "transactionsCache");
     }
 }
