77eab25f701b0e963a4c683187d2fe4da424f983
Fangsangik <hwangsangik@gmail.com>
2024-09-04 15:45:47 +0900

    feat : "dto -> entity"

--- src/main/java/miniproject/fintech/service/MemoryMemberService.java
+++ src/main/java/miniproject/fintech/service/MemoryMemberService.java
@@ -7,6 +7,7 @@ import miniproject.fintech.domain.BankMember;
 import miniproject.fintech.dto.AccountDto;
 import miniproject.fintech.dto.BankMemberDto;
 import miniproject.fintech.dto.DtoConverter;
+import miniproject.fintech.dto.EntityConverter;
 import miniproject.fintech.error.CustomError;
 import miniproject.fintech.repository.MemberRepository;
 import miniproject.fintech.type.ErrorType;
@@ -27,37 +28,37 @@ import static miniproject.fintech.type.ErrorType.*;
 public class MemoryMemberService {
 
     private final MemberRepository memberRepository;
-    private final PasswordEncoder  passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
     private final DtoConverter converter;
+    private final EntityConverter entityConverter;
 
-    // ID로 조회하여 DTO로 반환
-    public Optional<BankMemberDto> findById(Long id) {
+    // ID로 조회하여 반환
+    public BankMember findById(Long id) {
         log.info("ID로 은행 회원 찾기: {}", id);
 
-        BankMember bankMember = memberRepository.findById(id)
-                .orElseThrow(() -> new CustomError(MEMBER_NOT_FOUND));
+        if (id == null) {
+            log.error("회원 ID가 null입니다.");
+            throw new CustomError(MEMBER_NOT_FOUND); // CustomError 예외 던지기
+        }
 
-        log.info("은행 회원 찾기 성공: {}", bankMember);
-        return Optional.ofNullable(converter.convertToBankMemberDto(bankMember));
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new CustomError(MEMBER_NOT_FOUND));
     }
 
-    // 모든 회원 목록을 DTO 목록으로 반환
-    public List<BankMemberDto> findAll() {
+    // 모든 회원 목록을 반환
+    public List<BankMember> findAll() {
         log.info("모든 은행 회원 찾기");
-        List<BankMember> members = memberRepository.findAll();
-        return members.stream()
-                .map(converter::convertToBankMemberDto)
-                .collect(Collectors.toList());
+        return memberRepository.findAll();
     }
 
     // 비밀번호 변경
     public void userChangePassword(Long bankMemberId, String oldPassword, String newPassword) {
-        log.info("사용자 비밀번호 변경 시도 : 사용자 ID {}", bankMemberId);
+        log.info("사용자 비밀번호 변경 시도: 사용자 ID {}", bankMemberId);
         BankMember member = memberRepository.findById(bankMemberId)
                 .orElseThrow(() -> new CustomError(MEMBER_NOT_FOUND));
 
         if (!passwordEncoder.matches(oldPassword, member.getPassword())) {
-            log.error("현재 비밀번호가 일치하지 않습니다 : 사용자 ID {}", bankMemberId);
+            log.error("현재 비밀번호가 일치하지 않습니다: 사용자 ID {}", bankMemberId);
             throw new CustomError(PASSWORD_INCORRECT);
         }
 
@@ -77,35 +78,29 @@ public class MemoryMemberService {
         validationCreateNewMember(bankMemberDto);
 
         String encodedPassword = passwordEncoder.encode(bankMemberDto.getPassword());
-
-        // 기본 역할 'USER'를 설정
-        String memberRoles = "USER";
-
-        // 추가 역할이 있으면 쉼표로 구분하여 설정
-        if (roles != null && !roles.isEmpty()) {
-            memberRoles = String.join(",", roles); // Set을 쉼표로 구분된 문자열로 변환
-        }
+        String memberRoles = roles != null && !roles.isEmpty() ? roles : "USER";
 
         BankMember newBankMember = BankMember.builder()
                 .name(bankMemberDto.getName())
                 .email(bankMemberDto.getEmail())
-                .isActive(true)
+                .isActive(bankMemberDto.isActive())
+                .amount(bankMemberDto.getAmount())
+                .age(bankMemberDto.getAge())
+                .birth(bankMemberDto.getBirth())
                 .password(encodedPassword)
                 .address(bankMemberDto.getAddress())
                 .createdAt(bankMemberDto.getCreatedAt())
-                .accountNumber(bankMemberDto.getAccountNumber())
                 .roles(memberRoles)
                 .build();
 
         BankMember savedMember = memberRepository.save(newBankMember);
         log.info("새 은행 회원 생성 성공: {}", savedMember);
-        return converter.convertToBankMemberDto(savedMember);
+        return savedMember;
     }
 
     @Transactional
-    public BankMemberDto updateMember(Long id, BankMemberDto updatedMemberDto) {
+    public BankMember updateMember(Long id, BankMemberDto updatedMemberDto) {
         log.info("은행 회원 업데이트 요청: ID - {}, 내용 - {}", id, updatedMemberDto);
-
         BankMember existingMember = validationOfId(id);  // 기존 회원 조회
 
         // 비밀번호 변경 로직 추가
@@ -126,9 +121,8 @@ public class MemoryMemberService {
         BankMember savedMember = memberRepository.save(existingMember);
         log.info("은행 회원 업데이트 성공: {}", savedMember);
 
-        return converter.convertToBankMemberDto(savedMember);
+        return savedMember;
     }
-
 
     // 회원 삭제 메서드
     @Transactional
@@ -151,29 +145,26 @@ public class MemoryMemberService {
     }
 
     @Transactional(readOnly = true)
-    public List<AccountDto> findAccountByMemberId(Long id) {
+    public List<Account> findAccountByMemberId(Long id) {
         log.info("회원 ID로 계좌 찾기: ID - {}", id);
         BankMember member = memberRepository.findById(id)
                 .orElseThrow(() -> new CustomError(MEMBER_NOT_FOUND));
 
-        return member.getAccounts().stream()
-                .map(converter::convertToAccountDto)
-                .collect(Collectors.toList());
+        return member.getAccounts();
     }
 
     @Transactional(readOnly = true)
-    public BankMemberDto getBankMemberById(Long bankMemberId) {
+    public BankMember getBankMemberById(Long bankMemberId) {
         log.info("회원 ID로 은행 회원 찾기: ID - {}", bankMemberId);
         BankMember member = memberRepository.findById(bankMemberId)
                 .orElseThrow(() -> new CustomError(MEMBER_NOT_FOUND));
 
-        return converter.convertToBankMemberDto(member);
+        return member;
     }
 
-    public Page<BankMemberDto> findAll(Pageable pageable) {
+    public Page<BankMember> findAll(Pageable pageable) {
         log.info("페이지를 사용하여 모든 은행 회원 찾기: {}", pageable);
-        Page<BankMember> page = memberRepository.findAll(pageable);
-        return page.map(converter::convertToBankMemberDto);
+        return memberRepository.findAll(pageable);
     }
 
     public void changePassword(Long id, String password) {
@@ -191,29 +182,26 @@ public class MemoryMemberService {
     }
 
     private void validationCreateNewMember(BankMemberDto bankMember) {
-        log.info("새 은행 회원 검증 중: 이메일 - {}, 계좌번호 - {}",
-                bankMember.getEmail(), bankMember.getAccountNumber());
-
-        Optional<BankMember> existingMemberByEmail = memberRepository.findByEmail(bankMember.getEmail());
-        if (existingMemberByEmail.isPresent()) {
+        Optional<BankMember> byEmail = memberRepository.findByEmail(bankMember.getEmail());
+        log.info("새 은행 회원 검증 중: 이메일 - {}, 계좌번호 - {}", byEmail);
+        if (byEmail.isPresent()) {
             log.error("이미 사용 중인 이메일: {}", bankMember.getEmail());
             throw new CustomError(EMAIL_DUPLICATE);
-        }
-
-        Optional<BankMember> existingMemberByAccountNumber = memberRepository.findByAccountNumber(bankMember.getAccountNumber());
-        if (existingMemberByAccountNumber.isPresent()) {
-            log.error("이미 사용 중인 계좌번호: {}", bankMember.getAccountNumber());
-            throw new CustomError(ACCOUNT_NUMBER_DUPLICATE);
         }
     }
 
     private void deleteValidation(Long id, String password, BankMember bankMember) {
-        if (bankMember.getPassword().equals(password)) {
-            log.info("비밀번호 일치. 은행 회원 삭제: ID - {}", id);
-            memberRepository.deleteById(id);
-        } else {
+        if (bankMember.getPassword() == null) {
+            log.error("비밀번호가 null입니다: 은행 회원 ID - {}", id);
+            throw new CustomError(PASSWORD_MUST_NOT_NULL);
+        }
+
+        if (!bankMember.getPassword().equals(password)) {
             log.error("비밀번호 불일치: 은행 회원 ID - {}", id);
             throw new CustomError(PASSWORD_INCORRECT);
         }
+
+        log.info("비밀번호 일치. 은행 회원 삭제: ID - {}", id);
+        memberRepository.deleteById(id);
     }
 }
