7b0707c7ef683736f1fe2115702f41120aedc623
Fangsangik <hwangsangik@gmail.com>
2024-09-06 23:02:48 +0900

    feat : "findByAdminId"

--- src/main/java/miniproject/fintech/repository/AdminRepository.java
+++ src/main/java/miniproject/fintech/repository/AdminRepository.java
@@ -3,5 +3,8 @@ package miniproject.fintech.repository;
 import miniproject.fintech.domain.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByAdminId(String username);
 }
