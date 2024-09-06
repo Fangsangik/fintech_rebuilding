6476251d3bb033c4ca5e4ec3127e6919902c4b7f
Fangsangik <hwangsangik@gmail.com>
2024-09-06 23:13:18 +0900

    refactor : "id -> userId;"

--- src/main/java/miniproject/fintech/service/DiscountService.java
+++ src/main/java/miniproject/fintech/service/DiscountService.java
@@ -6,6 +6,7 @@ import miniproject.fintech.domain.BankMember;
 import miniproject.fintech.error.CustomError;
 import miniproject.fintech.repository.DepositRepository;
 import miniproject.fintech.repository.DiscountRepository;
+import miniproject.fintech.repository.MemberRepository;
 import miniproject.fintech.type.Grade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,31 +23,33 @@ public class DiscountService {
 
     private static final double DISCOUNT_RATE = 0.01;
     private final DepositRepository depositRepository;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public DiscountService(DiscountRepository discountRepository, DepositRepository depositRepository) {
+    public DiscountService(DiscountRepository discountRepository, DepositRepository depositRepository, MemberRepository memberRepository) {
         this.discountRepository = discountRepository;
         this.depositRepository = depositRepository;
+        this.memberRepository = memberRepository;
     }
 
-    public void applyDiscount(Long memberId, String password) {
-        log.info("회원 ID {}에 대한 할인 적용 요청", memberId);
+    public void applyDiscount(String userId, String password) {
+        log.info("회원 ID {}에 대한 할인 적용 요청", userId);
 
         // 회원을 조회합니다.
-        BankMember bankMember = discountRepository.findById(memberId)
+        BankMember bankMember = memberRepository.findByUserId(userId)
                 .orElseThrow(() -> {
-                    log.error("회원 조회 실패: ID = {}", memberId);
+                    log.error("회원 조회 실패: ID = {}", userId);
                     return new CustomError(MEMBER_NOT_FOUND);
                 });
 
         // 비밀번호와 등급 확인
         if (!bankMember.getPassword().equals(password)) {
-            log.error("비밀번호 불일치: 회원 ID = {}", memberId);
+            log.error("비밀번호 불일치: 회원 ID = {}", userId);
             throw new CustomError(PASSWORD_INCORRECT);
         }
 
         if (bankMember.getGrade() == null || !bankMember.getGrade().equals(Grade.VIP)) {
-            log.error("회원 등급 불일치: 회원 ID = {}, 등급 = {}", memberId, bankMember.getGrade());
+            log.error("회원 등급 불일치: 회원 ID = {}, 등급 = {}", userId, bankMember.getGrade());
             throw new CustomError(GRADE_NOT_VIP);
         }
 
@@ -59,7 +62,7 @@ public class DiscountService {
         discountRepository.save(bankMember);
 
         log.info("할인 적용 완료: 회원 ID = {}, 원래 금액 = {}, 할인 금액 = {}, 새로운 금액 = {}",
-                memberId, originalAmount, discountAmount, newAmount);
+                userId, originalAmount, discountAmount, newAmount);
     }
 }
 
