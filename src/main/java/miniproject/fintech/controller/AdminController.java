0331020f388a2d4137fdbf8be80f542c1bfb2908
Fangsangik <hwangsangik@gmail.com>
2024-09-06 23:02:16 +0900

    refactor : "cache 값 설정, entity -> dto로 변경, id -> string"

--- src/main/java/miniproject/fintech/controller/AdminController.java
+++ src/main/java/miniproject/fintech/controller/AdminController.java
@@ -2,12 +2,11 @@ package miniproject.fintech.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import miniproject.fintech.domain.Admin;
-import miniproject.fintech.domain.BankMember;
-import miniproject.fintech.domain.Transaction;
 import miniproject.fintech.dto.AdminDto;
 import miniproject.fintech.dto.BankMemberDto;
+import miniproject.fintech.dto.TransactionDto;
 import miniproject.fintech.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
@@ -19,78 +18,72 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/admin")
-@RequiredArgsConstructor
 public class AdminController {
 
     private final AdminService adminService;
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping("/allUsers")
-    @Cacheable(value = "adminCache", key = "'allUsers'")
-    public ResponseEntity<List<BankMember>> getAllUsers(
-            @RequestParam(defaultValue = "0") int pageNum,
-            @RequestParam(defaultValue = "5") int pageSize) {
-        List<BankMember> allBankMembers = adminService.getAllBankMembers(pageNum, pageSize);
-        return ResponseEntity.ok(allBankMembers);
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("/toggle/{id}")
-    @CacheEvict(value = "adminCache", key = "'allUsers'") // 캐시 무효화
-    public ResponseEntity<BankMember> toggleBankMember(
-            @PathVariable Long id,
-            @RequestParam boolean isActive) {
-        BankMember bankMemberDto = adminService.toggleUserActivation(id, isActive);
-        return ResponseEntity.ok(bankMemberDto);
+    @GetMapping("/allMembers")
+    @Cacheable(value = "adminCache", key = "'allMembers'")
+    public ResponseEntity<List<BankMemberDto>> getAllBankMembers(
+            @RequestParam int pageNum,
+            @RequestParam int pageSize) {
+        List<BankMemberDto> allMembers = adminService.getAllBankMembers(pageNum, pageSize);
+        return ResponseEntity.ok(allMembers);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("/update/{id}")
+    @PostMapping("/toggleActivation/{userId}")
     @CacheEvict(value = "adminCache", key = "'allUsers'")
-    public ResponseEntity<BankMember> update(
-            @PathVariable Long id,
-            @RequestBody BankMemberDto bankMemberDto) {
-        BankMember updated = adminService.updateUserDetails(id, bankMemberDto);
-        return ResponseEntity.ok(updated);
+    public ResponseEntity<BankMemberDto> toggleUserActivation(
+            @PathVariable String userId,
+            @RequestParam boolean isActive) {
+        BankMemberDto updatedUser = adminService.toggleUserActivation(userId, isActive);
+        return ResponseEntity.ok(updatedUser);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping("/getTransaction")
-    @Cacheable(value = "adminCache", key = "'transactions'")
-    public ResponseEntity<List<Transaction>> getTransaction(
-            @RequestParam(defaultValue = "0") int pageNum,
-            @RequestParam(defaultValue = "5") int pageSize) {
-        List<Transaction> allTransactions = adminService.getAllTransactions(pageNum, pageSize);
+    @GetMapping("/allTransactions")
+    @Cacheable(value = "adminCache", key = "'allTransactions'")
+    public ResponseEntity<List<TransactionDto>> getAllTransactions(
+            @RequestParam int pageNum,
+            @RequestParam int pageSize) {
+        List<TransactionDto> allTransactions = adminService.getAllTransactions(pageNum, pageSize);
         return ResponseEntity.ok(allTransactions);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("/updateAdmin/{id}")
-    @CacheEvict(value = "adminCache", key = "'adminDetails'") // 특정 캐시 키 무효화
-    public ResponseEntity<Admin> updateAdmin(
-            @PathVariable Long id,
+    @PostMapping("/updateAdmin/{adminId}")
+    @CacheEvict(value = "adminCache", key = "'adminDetails'")
+    public ResponseEntity<AdminDto> updateAdmin(
+            @PathVariable String adminId,
             @RequestBody AdminDto adminDto) {
-        Admin updatedAdmin = adminService.updateAdmin(id, adminDto);
+        AdminDto updatedAdmin = adminService.updateAdmin(adminId, adminDto);
         return ResponseEntity.ok(updatedAdmin);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/changeUserPassword")
-    @CacheEvict(value = "adminCache", key = "'allUsers'") // 사용자의 비밀번호 변경 시 캐시 무효화
+    @CacheEvict(value = "adminCache", key = "'allUsers'")
     public ResponseEntity<Void> changeUserPassword(
-            @RequestParam Long adminId,
-            @RequestParam Long userId,
+            @RequestParam String adminId,
+            @RequestParam String userId,
             @RequestParam String newPassword) {
         adminService.adminChangeUserPassword(adminId, userId, newPassword);
         return ResponseEntity.ok().build();
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("/changePassword/{id}")
+    @PostMapping("/changePassword/{adminId}")
     public ResponseEntity<Void> changeAdminPassword(
-            @PathVariable Long id, // @PathVariable로 변경
+            @PathVariable String adminId,
             @RequestParam String newPassword) {
-        adminService.changeAdminPassword(id, newPassword);
+        adminService.changeAdminPassword(adminId, newPassword);
         return ResponseEntity.ok().build();
     }
 }
