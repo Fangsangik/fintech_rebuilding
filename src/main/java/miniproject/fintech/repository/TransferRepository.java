87ec581924e67e00e0bd655e5a6ca6f6bd2305e2
Fangsangik <hwangsangik@gmail.com>
2024-09-06 23:25:04 +0900

    feat : "findBySourceAccountIdOrDestinationAccountId -> findBySourceAccountNumberOrDestinationAccountNumber"

--- src/main/java/miniproject/fintech/repository/TransferRepository.java
+++ src/main/java/miniproject/fintech/repository/TransferRepository.java
@@ -1,5 +1,6 @@
 package miniproject.fintech.repository;
 
+import miniproject.fintech.domain.Account;
 import miniproject.fintech.domain.Transfer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,13 +9,14 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TransferRepository extends JpaRepository<Transfer, Long> {
     List<Transfer> findByTransferAtBetween(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
 
-    List<Transfer> findBySourceAccountId(Long accountId);
     Page<Transfer> findAll(Pageable pageable);
 
-    List<Transfer> findBySourceAccountIdOrDestinationAccountId(Long accountId, Long accountId1);
+    List<Transfer> findBySourceAccountNumberOrDestinationAccountNumber(String accountNumber1, String accountNumber2);
+
 }
