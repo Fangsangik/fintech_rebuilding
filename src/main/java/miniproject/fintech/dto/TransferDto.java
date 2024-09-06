d8eec527a922fbd772974ae09c821c8b81c2d8cd
Fangsangik <hwangsangik@gmail.com>
2024-09-06 23:24:46 +0900

    feat : "  private BankMemberDto bankMemberDto;             private Long transactionId;,                 private String sourceAccountNumber;                 private String destinationAccountNumber;"

--- src/main/java/miniproject/fintech/dto/TransferDto.java
+++ src/main/java/miniproject/fintech/dto/TransferDto.java
@@ -2,30 +2,27 @@ package miniproject.fintech.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import miniproject.fintech.type.TransferStatus;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @Builder(toBuilder = true) //setter 사용을 줄여보기 위해 사용
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransferDto {
-
-    @NotNull
-    @Min(1)
     private Long id;
 
     private long transferAmount;
     private LocalDateTime transferAt;
 
-    private long sourceAccountId;
-    private long destinationAccountId;
+    private String sourceAccountNumber;
+    private String destinationAccountNumber;
     private TransferStatus transferStatus;
 
+    private BankMemberDto bankMemberDto;
+    private Long transactionId;
     private String message;
 }
