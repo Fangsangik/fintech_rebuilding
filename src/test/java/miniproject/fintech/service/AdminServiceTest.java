2d132f53a767c36928545b98543e446a1daf8eb1
Fangsangik <hwangsangik@gmail.com>
2024-09-06 23:03:47 +0900

    feat : ""

--- src/test/java/miniproject/fintech/service/AdminServiceTest.java
+++ src/test/java/miniproject/fintech/service/AdminServiceTest.java
@@ -6,7 +6,9 @@ import miniproject.fintech.dto.DtoConverter;
 import miniproject.fintech.repository.MemberRepository;
 import miniproject.fintech.repository.TransactionRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,6 +17,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AdminServiceTest {
 
     @Autowired
@@ -34,7 +37,9 @@ class AdminServiceTest {
 
     @BeforeEach
     void setUp() {
+        memberRepository.deleteAll();
         BankMember bankMember = memberRepository.save(BankMember.builder()
+                .userId("test")
                 .name("test")
                 .email("test@test.com")
                 .isActive(true)
@@ -47,7 +52,7 @@ class AdminServiceTest {
     void testGetAllBankMembers() {
         int pageNum = 0;
         int pageSize = 10;
-        List<BankMember> rst = adminService.getAllBankMembers(pageNum, pageSize);
+        List<BankMemberDto> rst = adminService.getAllBankMembers(pageNum, pageSize);
 
         assertNotNull(rst);
         assertFalse(rst.isEmpty());
@@ -56,11 +61,11 @@ class AdminServiceTest {
     @Test
     void testToggleUserActivation() {
         BankMember bankMember = memberRepository.findAll().get(0);
-        Long bankMemberId = bankMember.getId();
+        String bankMemberId = bankMember.getUserId();
         boolean activeStatus = bankMember.isActive();
 
         adminService.toggleUserActivation(bankMemberId, !activeStatus);
-        BankMember updateBankMember = memberRepository.findById(bankMemberId).orElse(null);
+        BankMember updateBankMember = memberRepository.findByUserId(bankMemberId).orElse(null);
 
         assertNotNull(updateBankMember);
         assertEquals(!activeStatus, updateBankMember.isActive());
@@ -70,14 +75,14 @@ class AdminServiceTest {
     void testUpdateUserDetails() {
         // Arrange
         BankMember bankMember = memberRepository.findAll().get(0);
-        Long memberId = bankMember.getId();
+        String memberId = bankMember.getUserId();
         BankMemberDto bankMemberDto = new BankMemberDto();
         bankMemberDto.setName("Updated Name");
         bankMemberDto.setEmail("updated@example.com");
 
         // Act
-        BankMember updatedMemberDto = adminService.updateUserDetails(memberId, bankMemberDto);
-        BankMember updatedBankMember = memberRepository.findById(memberId).orElse(null);
+        BankMemberDto updatedMemberDto = adminService.updateUserDetails(memberId, bankMemberDto);
+        BankMember updatedBankMember = memberRepository.findByUserId(memberId).orElse(null);
 
         // Assert
         assertNotNull(updatedMemberDto);
